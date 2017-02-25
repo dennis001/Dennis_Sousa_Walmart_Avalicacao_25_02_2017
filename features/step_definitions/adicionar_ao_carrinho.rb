c0ba634928944e3a81452749d40d6c322342c500
Dennis Sousa <Dennis Sousa>
2017-02-25 19:28:51 -0300

    adicionar ao carrinho

---
+++ features/step_definitions/adicionar_ao_carrinho.rb
@@ -0,0 +1,19 @@
+Dado(/^que esteja na home do Walmart$/) do
+  visit "http://www.walmart.com.br"
+end
+
+Quando(/^pesquisar o produto$/) do
+  fill_in "suggestion-search", :with => "fralda pampers supreme care g meninas"
+  click_button("Procurar")
+end
+
+Então(/^verificar se está na pagina do produto$/) do
+  assert_text("Fralda Huggies Turma Monica Supreme Care Meninas Tam G - 32 Unidades")
+  click_on("Fralda Huggies Turma Monica Supreme Care Meninas Tam G - 32 Unidades", match: :first)
+  click_button("Adicionar ao carrinho")
+  sleep 5
+end
+
+Então(/^validar o produto no carrinho$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
