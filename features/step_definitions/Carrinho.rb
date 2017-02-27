349e8d382a25f7b1afbea67f9cc283647823d74a
Dennis Sousa <Dennis Sousa>
2017-02-27 13:15:17 -0300

    finalizando boleto

--- features/step_definitions/Carrinho.rb
+++ features/step_definitions/Carrinho.rb
@@ -22,31 +22,33 @@ end
 
 Dado(/^que o produto já esta no carrinho de compras$/) do
   click_button("Finalizar compra", match: :first)
-  login_window = page.driver.find_window('iframeLogin')
-  	main_window = page.driver.find_window('')
-  #visit "https://www2.walmart.com.br/login?modal=true&continue=https://connect.walmart.com.br/connect/authorize?response_type=code&client_id=walmart_checkout&redirect_uri=https://www2.walmart.com.br/checkout/services/transaction/oauth/callback/walmart_checkout"
+  #login_window = page.driver.find_window('iframeLogin')
+  	#main_window = page.driver.find_window('')
+  visit "https://www2.walmart.com.br/login?modal=true&continue=https://connect.walmart.com.br/connect/authorize?response_type=code&client_id=walmart_checkout&redirect_uri=https://www2.walmart.com.br/checkout/services/transaction/oauth/callback/walmart_checkout"
   #new_window = window_opened_by {click_link'https://www2.walmart.com.br/login?modal=true&continue=https://connect.walmart.com.br/connect/authorize?response_type=code&client_id=walmart_checkout&redirect_uri=https://www2.walmart.com.br/checkout/services/transaction/oauth/callback/walmart_checkout'}
 	#within_window new_window do
-  #assert_text("Já tem cadastro?")
- 	page.within_window(login_window) do
+  assert_text("Já tem cadastro?")
+ 	#page.within_window(login_window) do
  	
 	  	fill_in "signinField", :with => "dennisdesousa.ds@gmail.com"
   		fill_in "password", :with => "D.632256"
   		click_button("Entrar")
- 	#visit "https://www2.walmart.com.br/checkout/content/#chooseAddress"
- end
+ 	visit "https://www2.walmart.com.br/checkout/content/#chooseAddress"
+ #end
 end
 
 Quando(/^os dados de login já tenham sido preenchidos$/) do
   click_button("Entregar nesse endereço")
-  click_button("Continuar", match: :first)
+  assert_text("Entrega em até 5 dias úteis")
+  
 end
 
 Entao(/^conclua a compra selecionando os dados de entrega$/) do
-  click_link("Boleto bancário")
-  click_button("Finalizar compra")
+  find("#choose-delivery-continue-link").click
 end
 
 Entao(/^gere o boleto$/) do
-  #assert_text # Write code here that turns the phrase above into concrete actions
+ assert_text("Boleto bancário")
+ find(".active").click
+ # click_button("Finalizar compra")
 end                                                        
